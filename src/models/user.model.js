eb97f5f24dad4b9e073e4a60181dac336c0dbd11
yalsik <msdegoat@gmail.com>
2024-09-08 22:09:00 +0530

    register controller logic + bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = brcypt.hash(this.password, 10)
+    this.password = await brcypt.hash(this.password, 10)
     next()
 })
 
